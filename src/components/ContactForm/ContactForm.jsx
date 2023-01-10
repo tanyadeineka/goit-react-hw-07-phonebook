214f5c299744a1446f0e11a33e3d0b27d13b7f6a
Tetiana Deineka <tata4ka666@i.ua>
2023-01-10 21:46:24 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,58 +1,65 @@
 import css from "./ContactForm.module.css";
-import { useState } from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from "redux/contactsOperation";
+import { nanoid } from "nanoid";
 
 export const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+  const contacts = useSelector(state => state.contacts.items);
+  const idName = nanoid();
+  const idNumber = nanoid();
 
   const dispatch = useDispatch();
 
   const handleFormSubmit = event => {
     event.preventDefault();
-    dispatch(addContact({ name, number }));
-    setName('');
-    setNumber('');
-  };
+    
+    const object = {
+      [event.target.elements.name.name]: event.target.elements.name.value,
+      [event.target.elements.number.name]: event.target.elements.number.value,
+    };
 
-  const handleChange = event => {
-    const { name, value } = event.currentTarget;
-        switch (name) {
-            case "name":
-                setName(value)
-                break;
-            case "number":
-                setNumber(value)
-                break;
-            default:
-                return;
-         }
+    const includesName = contacts.find(
+      contact =>
+        contact.name.toLowerCase() ===
+        event.target.elements.name.value.toLowerCase()
+    );
+
+    if (includesName) {
+      alert(`${event.target.elements.name.value} is already in contacts`);
+      event.target.reset();
+      return;
+    }
+
+    dispatch(addContact(object));
+
+    event.target.reset();
   };
 
   return (
     <form className={css.form} onSubmit={handleFormSubmit}>
-      <label className={css.labelForm}>Name</label>
+      <label className={css.labelForm} htmlFor={idName}>
+        Name
+      </label>
       <input
         className={css.nameForm}
+        id={idName}
         type="text"
         name="name"
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
-        value={name}
-        onChange={handleChange}
       />
-      <label className={css.labelForm}>Number</label>
+      <label className={css.labelForm} htmlFor={idNumber}>
+        Number
+      </label>
       <input
         className={css.numberForm}
+        id={idNumber}
         type="tel"
         name="number"
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
-        value={number}
-        onChange={handleChange}
       />
       <button type="submit" className={css.formBtn}>
         Add contact
