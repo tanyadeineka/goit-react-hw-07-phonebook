2954294a84c59247496e5c619124af7ea7febc1d
Tetiana Deineka <tata4ka666@i.ua>
2023-01-10 15:29:52 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,21 @@
 import css from "./ContactList.module.css";
-import { removeContact } from 'redux/contactsSlice';
+import { useEffect } from 'react';
+import { fetchContact, deleteContact } from '../../redux/contactsOperation';
 import { useDispatch, useSelector } from 'react-redux';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(state => state.contacts);
-  const filterQuery = useSelector(state => state.filter);
 
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLocaleLowerCase().includes(filterQuery.toLocaleLowerCase())
+  const contacts = useSelector(state => state.contacts.items);
+
+  useEffect(() => {
+    dispatch(fetchContact());
+  }, [dispatch]);
+
+  const filterQuery = useSelector(state => state.filter.filter);
+
+  const filteredContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(filterQuery.toLowerCase())
   );
     return (
         <div className={css.contactContainer}>
@@ -16,7 +23,7 @@ export const ContactList = () => {
                 {filteredContacts.map(contact => (
                   <li key={contact.id} className={css.contactsItem}>
                     {contact.name}: {contact.number}
-                    <button className={css.deleteBtn} type="button" onClick={() => dispatch(removeContact(contact))}>Delete</button>
+                    <button className={css.deleteBtn} type="button" onClick={() => dispatch(deleteContact(contact.id))}>Delete</button>
                   </li>
                 ))}
             </ul>
