c260d84ca2e4392ae9669b9f51070dbe6e482e37
Tetiana Deineka <tata4ka666@i.ua>
2023-01-10 21:46:32 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,17 +1,22 @@
 import css from "./Filter.module.css";
 import { useDispatch, useSelector } from 'react-redux';
 import { filterContact } from 'redux/filterSlice';
+import { nanoid } from "nanoid";
 
 export const Filter = () => {
     const dispatch = useDispatch();
 
-    const filter = useSelector(state => state.filter.filter);
+  const filter = useSelector(state => state.filter.filter);
+  const idFilter = nanoid();
 
     return (
       <div className={css.filterContainer}>
-        <label className={css.labelFilter}>Find contacts by name</label>
+        <label className={css.labelFilter} htmlFor={idFilter}>
+          Find contacts by name
+        </label>
         <input
           className={css.inputFilter}
+          id={idFilter}
           type="text"
           name="filter"
           value={filter}
